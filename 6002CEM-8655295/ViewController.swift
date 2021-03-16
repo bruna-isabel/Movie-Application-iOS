2b2c6b3bad6af05ad4cbeb752764a03ef24ac5bd
ricardob <ricardob@coventry.ac.uk>
2021-03-16 13:01:58 +0000

    Initial Commit

---
+++ 6002CEM-8655295/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6002CEM-8655295
+//
+//  Created by Bruna Coimbra on 16/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 6002CEM-8655295Tests/_002CEM_8655295Tests.swift
@@ -0,0 +1,33 @@
+//
+//  _002CEM_8655295Tests.swift
+//  6002CEM-8655295Tests
+//
+//  Created by Bruna Coimbra on 16/03/2021.
+//
+
+import XCTest
+@testable import _002CEM_8655295
+
+class _002CEM_8655295Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
