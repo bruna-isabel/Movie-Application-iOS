29f2c921b1ed33608b1c7916614b28c6c7bc2ea5
ricardob <ricardob@coventry.ac.uk>
2021-03-16 17:04:22 +0000

    Sign Up Main Image

--- 6002CEM-8655295/ViewController.swift
+++ 6002CEM-8655295/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var PasswordTextField: UITextField!
+    @IBOutlet var SignInButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        usernameTextField.layer.cornerRadius = 22
+        PasswordTextField.layer.cornerRadius = 22
+        SignInButton.layer.cornerRadius = 22
+        
     }
 
 
