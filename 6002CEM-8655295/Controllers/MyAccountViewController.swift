9ce7af1ee03463d0b8722be7f956d50e660e9b1b
ricardob <ricardob@coventry.ac.uk>
2021-04-11 17:09:31 +0100

    Image Picker is working. User can now update image but it's not saving to database

--- 6002CEM-8655295/Controllers/MyAccountViewController.swift
+++ 6002CEM-8655295/Controllers/MyAccountViewController.swift
@@ -24,6 +24,11 @@ class MyAccountViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func uploadImageButton(_ sender: Any) {
+        showImagePickerActionSheet()
+
+    }
     @IBAction func saveDetailsTapped(_ sender: Any) {
         //Saves Image in DB
     }
@@ -48,4 +53,59 @@ class MyAccountViewController: UIViewController {
         self.view.window?.makeKeyAndVisible()
         
         }
+}
+
+extension MyAccountViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    func showImagePickerActionSheet() {
+        
+        let alert = UIAlertController(title: "Choose your prefered method", message: nil, preferredStyle: .actionSheet)
+        
+            
+        alert.addAction(UIAlertAction(title: "Choose From Library", style: .default) { (action) in
+            self.showImagePicker(sourceType: .photoLibrary)
+        })
+        
+        alert.addAction(UIAlertAction(title: "Take from Camera", style: .default) { (action) in
+                
+            if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+                let alertController = UIAlertController(title: nil, message: "Device has no camera.", preferredStyle: .alert)
+
+                let okAction = UIAlertAction(title: "Alright", style: .default, handler: { (alert: UIAlertAction!) in
+                })
+
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+            
+            } else {
+                self.showImagePicker(sourceType: .camera)
+            }
+           
+        })
+        
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true)
+    }
+    
+    func showImagePicker(sourceType: UIImagePickerController.SourceType) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        //can edit image before choosing it
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = sourceType
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        //Handling both types of image
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            userImage.image = editedImage
+        } else if let  originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            userImage.image = originalImage
+        dismiss(animated: true, completion: nil)
+        }
+    }
 }
